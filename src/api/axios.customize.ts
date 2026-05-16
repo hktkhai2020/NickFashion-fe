fb915f9297134bbfb804c0ea52813f1ac3372db3
Nguyen Van Khoi <sinhloc@gmail.com>
2026-05-16 09:35:16 +0700

    fix: update axios config

--- src/api/axios.customize.ts
+++ src/api/axios.customize.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_BACKEND_URL || "https://api.nickfashion.asia";
+  import.meta.env.VITE_BACKEND_URL || "https://api.nickfashion.asia";  // newsnews
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
